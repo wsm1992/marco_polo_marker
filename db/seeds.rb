f70df794b252859cdca43f39286da6b2948ac8ee
Siuming <wsm1992@hotmail.com>
2017-06-11 21:02:06 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.where(id: 1, name: 'Siuming').first_or_create
+User.where(id: 2, name: 'Terence').first_or_create
+User.where(id: 3, name: 'Fred').first_or_create
+User.where(id: 4, name: 'Peter').first_or_create
+
+Role.where(id: 1, name: '控骰').first_or_create
+Role.where(id: 2, name: '攞合約').first_or_create
+Role.where(id: 3, name: '疊骰').first_or_create
+Role.where(id: 4, name: '資源王').first_or_create
