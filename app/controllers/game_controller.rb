249afda6bbb38c8c8adda5d7e0a4388b8150b8f2
Siuming <wsm1992@hotmail.com>
2017-06-13 13:34:44 -0400

    create game record when submit

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,20 @@
 class GameController < ApplicationController
   def new
+    @game = params[:game] || Game.new
   end
 
   def create
+    players = []
+    4.times do |i|
+      user_id = params[:user]["#{i}"].to_i
+      role_id = params[:role]["#{i}"].to_i
+      score = params[:score]["#{i}"].to_i
+      is_last_traveller = (params[:is_last_traveller]["#{i}"] == "1")
+      player = Player.create(user_id: user_id, role_id: role_id, score: score, is_last_traveller: is_last_traveller)
+      players << player 
+    end
+    game = Game.new
+    game.players = players
+    game.save!
   end
 end
