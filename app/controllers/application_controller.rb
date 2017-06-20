f52cee0bcd44b67e54c3940b62a8ba4cb23b75f7
Siuming <wsm1992@hotmail.com>
2017-06-20 15:13:20 -0400

    add error handler to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from StandardError, :with => :handle_error
+
+  def handle_error(e)
+    Rails.logger.error e.message
+    Rails.logger.error e.backtrace
+    result = {}
+    result[:error_code] = e.message
+    result[:error_msg] = e.backtrace.join("<br/>") unless e.class == RuntimeError 
+    respond_to do |format|
+      format.html{ render :text => result }
+    end
+  end
 end
