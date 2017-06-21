90c48f66a87cd932cb324f5e9edd904b6c2a07e7
Siuming <wsm1992@hotmail.com>
2017-06-21 10:27:36 -0400

    add is first mover checkbox and show in underline on list game

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -3,7 +3,8 @@ module GameHelper
   def player_info(player)
     concat label_tag("user", player.user.name)
     concat label_tag("role", "(#{player.role.name})")
-    style = player.is_last_traveller ? 'bold' : 'normal'
-    concat label_tag("score", "  #{player.score}", style: "font-weight:#{style}")
+    bold_style = player.is_last_traveller ? 'bold' : 'normal'
+    underline_style = player.is_first_mover ? 'underline' : 'normal'
+    concat label_tag("score", "  #{player.score}", style: "font-weight:#{bold_style}; text-decoration:#{underline_style}")
   end
 end
