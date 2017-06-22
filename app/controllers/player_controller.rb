6795f68afb8abd26bd06a11a6b845c93c1951298
Siuming <wsm1992@hotmail.com>
2017-06-22 17:01:55 -0400

    select all when check is no checked

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -4,8 +4,8 @@ class PlayerController < ApplicationController
     hash = {}
     hash[:user_id] = summary[:user_id] unless summary[:user_id] == ""
     hash[:role_id] = summary[:role_id] unless summary[:role_id] == ""
-    hash[:is_last_traveller] = summary[:is_last_traveller] == '1'
-    hash[:is_first_mover] = summary[:is_first_mover] == '1'
+    hash[:is_last_traveller] = true if summary[:is_last_traveller] == '1'
+    hash[:is_first_mover] = true if summary[:is_first_mover] == '1'
     @hash = hash
     @player_list = PlayerList.new(Player.where(hash))
   end
