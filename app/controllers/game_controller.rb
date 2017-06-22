96fbbd1f11fa038550ee9a9f1e9880a1081ba259
Siuming <wsm1992@hotmail.com>
2017-06-22 12:21:12 -0400

    add average score to list view

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -9,5 +9,10 @@ class GameController < ApplicationController
 
   def list
     @games = Game.all
+    @player_lists = []
+    (1..4).each do |i|
+      @player_lists << PlayerList.new(Player.where("user_id = ?", i))
+    end
+    @total_avg_score = @player_lists.map{|pl| pl.avg_score}.inject{|sum, s| sum + s}/4
   end
 end
