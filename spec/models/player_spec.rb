52e2aaad7d7fb9d0c054ef3edd7f6a33314169e3
Siuming <wsm1992@hotmail.com>
2017-06-23 07:12:15 -0400

    add get game cheking from player to test case

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -3,12 +3,18 @@ require 'rails_helper'
 RSpec.describe Player, type: :model do
   let(:user){ User.new(name: 'user') }
   let(:role){ Role.new(name: 'role') }
-  subject{ Player.new(user: user, role: role, score: 50, is_last_traveller: false, is_first_mover: true) }
+  let(:game){ Game.new }
+  subject{ Player.new(user: user, role: role, score: 50, is_last_traveller: false, is_first_mover: true, game: game) }
 
+  it { is_expected.to respond_to(:game) }
   it { is_expected.to respond_to(:user) }
   it { is_expected.to respond_to(:role) }
   it { is_expected.to respond_to(:score) }
   it { is_expected.to respond_to(:is_last_traveller) }
+
+  it 'get the game' do
+    expect(subject.game).to eq game
+  end
 
   it 'get the user' do
     expect(subject.user).to eq user
