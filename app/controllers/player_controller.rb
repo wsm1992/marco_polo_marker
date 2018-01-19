f6a72bd2e9fa33127e6a5d8747e1cf6dda3abbf5
Siuming <wsm1992@hotmail.com>
2018-01-19 08:04:31 +0000

    add season id to search columns

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,11 +1,12 @@
 class PlayerController < ApplicationController
   def summary
-    summary = params[:summary] || {user_id: "", role_id: "", is_last_traveller: '0', is_first_mover: '0'}
+    summary = params[:summary] || {user_id: "", role_id: "", is_last_traveller: '0', is_first_mover: '0', season_id: Season.max}
     hash = {}
     hash[:user_id] = summary[:user_id] unless summary[:user_id] == ""
     hash[:role_id] = summary[:role_id] unless summary[:role_id] == ""
     hash[:is_last_traveller] = true if summary[:is_last_traveller] == '1'
     hash[:is_first_mover] = true if summary[:is_first_mover] == '1'
+    hash[:season_id] = summary[:season_id] unless summary[:season_id] == ""
     @hash = hash
     @player_list = PlayerList.new(Player.where(hash))
   end
