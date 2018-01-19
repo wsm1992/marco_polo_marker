79f5edd6c91479ead9339354a327daeb7beb0099
Siuming <wsm1992@hotmail.com>
2018-01-19 09:25:48 +0000

    fix find all season bug

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -7,6 +7,7 @@ class PlayerController < ApplicationController
     hash[:role_id] = summary[:role_id] unless summary[:role_id] == ""
     hash[:is_last_traveller] = true if summary[:is_last_traveller] == '1'
     hash[:is_first_mover] = true if summary[:is_first_mover] == '1'
+    hash[:games] = {}
     hash[:games] = {season_id: summary[:season_id]} unless summary[:season_id] == ""
     @hash = hash
     @player_list = PlayerList.new(Player.joins(:game).where(hash))
