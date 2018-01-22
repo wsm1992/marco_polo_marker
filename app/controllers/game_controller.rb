f0232efa3e6ebe4027251be14b02cf8ab631f17c
Siuming <wsm1992@hotmail.com>
2018-01-22 02:22:55 +0000

    show the newest season on game list page

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -8,10 +8,10 @@ class GameController < ApplicationController
   end
 
   def list
-    @games = Game.all
+    @games = Game.where(season_id: Season.max)
     @player_lists = []
     (1..4).each do |i|
-      @player_lists << PlayerList.new(Player.where("user_id = ?", i))
+      @player_lists << PlayerList.new(Player.joins(:game).where(user_id: i, games:{season_id: Season.max}))
     end
     @total_avg_score = @player_lists.map{|pl| pl.avg_score}.inject{|sum, s| sum + s}/4
   end
